d703dc49a84432f97f87111bc9ea7556f0dc58ca
jhowell345 <jacob.howell@teamaol.com>
2015-06-30 12:35:10 -0400

    Combing first two modals

--- js/modal1.js
+++ js/modal1.js
@@ -1,16 +1,40 @@
 $(document).ready(function() {
-	$("#quotes1 .add_shortcode").on("click", addShortcode1);
-
-   function addShortcode1() {
-
-    // TODO - this is basic output from the form
-    var shortcode = $("#quotes1 form").serialize();
-    $("#comment").text(shortcode);
-    console.log("adding shortcode for Quotes1: " + shortcode);
-
-    // close the modal
-    $("#quotes1 .close-me").click();
-    console.log("closing modal")
+    $("#quoteorsharelinebutton").on("click", hideAll);
+        function hideAll() {
+            $(".reveal").hide()
     }
 
+    $("#quotesorshareline .quotebutton").on("click", hideShareLine);
+    
+    function hideShareLine() {
+        $(".reveal").show()
+        $(".shareline").hide()
+        $(".quote").show()
+        document.getElementById("selector").innerHTML = "Quote"       
+    }
+
+    $("#quotesorshareline .sharelinebutton").on("click", hideQuote);
+
+    function hideQuote() {
+        $(".reveal").show()
+        $(".quote").hide()
+        $(".shareline").show()
+        document.getElementById("selector").innerHTML = "Share Line"     
+    }
+    
+    $("#quotesorshareline .add_shortcode").on("click", addShortcode1);
+
+    function addShortcode1() {
+
+        // TODO - this is basic output from the form
+        var shortcode = $("#quotesorshareline form").serialize();
+        $("#comment").text(shortcode);
+        console.log("adding shortcode for Quotes1: " + shortcode);
+
+        // close the modal
+        $("#quotesorshareline .close-me").click();
+        console.log("closing modal")
+    }
  });
+
+    
